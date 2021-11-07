8ab93312097076100fe136027da2e89d379b9e16
Your Name <prathameshd419@gmail.com>
2021-11-07 20:47:50 +0530

    Simple bot commands
    Co-authored-by: Audrey Roy Greenfeld <aroy@alum.mit.edu>

--- index.js
+++ index.js
@@ -10,5 +10,23 @@ client.once("ready", () => {
   console.log("Ready!");
 });
 
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+ else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`,
+    );
+  }
+ else if (commandName === "user") {
+    await interaction.reply("User info.");
+  }
+});
+
 // Login to Discord with your client's token
 client.login(token);
